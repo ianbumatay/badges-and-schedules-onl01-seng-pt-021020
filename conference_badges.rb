247c756c6c330dbb37a0835877958b0e6e003b1a
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-16 04:26:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,6 @@
 # Write your code here.
+
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
@@ -7,14 +9,8 @@ badge_maker("Arel")
 speakers = ["Edsger, Ada, Charles, Alan, Grace, Linus, and Matz"]
 
 def batch_badge_creator(speakers)
-  speakers.each do |speaker|
-  puts "Hello, my name is #{speaker}"
+  speakers.each do |element|
+  return "Hello, my name is #{element}"
   end
 end
 batch_badge_creator(speakers)
-
-def assign_rooms(speakers)
-  speakers.each_with_index do |speaker, i|
-  puts "Hello, #{speaker}! You'll be assigned to room #{i}!"
-end
-assign_rooms(speakers)
