c75e9a1e98186f21331c6ad3b4ba8cbbe5f08cd0
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-16 04:42:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,3 +23,14 @@ def assign_rooms(speakers)
   return assign
 end
 assign_rooms(speakers)
+
+
+def printer(speakers)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
+end
