9d87b56395800350f56e0acaaaa1ff3c5fdf9eda
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-16 04:21:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,18 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 badge_maker("Arel")
+
+speakers = ["Edsger, Ada, Charles, Alan, Grace, Linus, and Matz"]
+
+def batch_badge_creator(speakers)
+  speakers.each do |speaker|
+  puts "Hello, my name is #{speaker}"
+  end
+end
+batch_badge_creator(speakers)
+
+def assign_rooms(speakers)
+  speakers.each_with_index do |speaker, i|
+  puts "Hello, #{speaker}! You'll be assigned to room #{i}!"
+end
+assign_rooms(speakers)
