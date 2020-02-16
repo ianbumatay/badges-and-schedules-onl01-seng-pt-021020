ce6a36377f30ae038bf3589e6ac6f75f6cbe10ef
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-16 04:28:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ speakers = ["Edsger, Ada, Charles, Alan, Grace, Linus, and Matz"]
 
 def batch_badge_creator(speakers)
   speakers.each do |element|
-  puts "Hello, my name is #{element}"
+  puts "Hello, my name is #{element}."
   end
 end
 batch_badge_creator(speakers)
